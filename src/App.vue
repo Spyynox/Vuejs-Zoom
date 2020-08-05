be628e1c50c5f4076049c73b337bafa15868cb13
Spyynox <maxime.mbiya-kiaku@supinternet.fr>
2020-08-05 14:01:12 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Zoom src="https://picsum.photos/1000/1000?image=142" src-large="https://picsum.photos/2000/2000?image=142"/>
+  </div>
+</template>
+
+<script>
+import Zoom from './components/Zoom.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Zoom
+  }
+}
+</script>
+
+<style>
+</style>
